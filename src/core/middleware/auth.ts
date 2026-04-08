be9fe18414327c49f00c2ab89853d9809ef3b54b
Lunatine <github@lunatine.dev>
2026-04-08 18:41:34 +0100

    refactor: Adhere to sensible standard of throwing errors

--- src/core/middleware/auth.ts
+++ src/core/middleware/auth.ts
@@ -12,7 +12,7 @@ export const isAuthenticated = async (
     try {
         await request.jwtVerify();
     } catch (e) {
-        return reply.unauthorized("Unauthorized");
+        throw reply.unauthorized("Unauthorized");
     }
 
     // Extract data from JWT
@@ -24,7 +24,7 @@ export const isAuthenticated = async (
             { githubId },
             "Whitelisting check failed in middleware",
         );
-        return reply.unauthorized("Access Denied: You are not whitelisted.");
+        throw reply.unauthorized("Access Denied: You are not whitelisted.");
     }
 
     // Validate session & fetch user
@@ -36,7 +36,7 @@ export const isAuthenticated = async (
     }).populate("userId");
 
     if (!session || !session.userId)
-        return reply.unauthorized("Session expired or user not found");
+        throw reply.unauthorized("Session expired or user not found");
 
     // We now have access to the user, attach it to the request for use in subsequent routes
     request.userDoc = session.userId as User;
